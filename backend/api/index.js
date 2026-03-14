3cb5356e940b71deb5528dbe3f0e690b9ae07085
GiridharanS1729 <giridharans1729@gmail.com>
2026-03-14 15:36:34 +0530

    Add Vercel backend serverless adapter

---
+++ backend/api/index.js
@@ -0,0 +1,9 @@
+import { createApp } from '../src/app.js';
+
+const appPromise = createApp();
+
+export default async function handler(req, res) {
+  const app = await appPromise;
+  await app.ready();
+  app.server.emit('request', req, res);
+}
